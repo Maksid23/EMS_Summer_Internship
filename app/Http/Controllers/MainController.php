169d0d027248c0fc2e40a3cbae01b80bcd6c3621
@prathamdarjimain2003 <prathamdarjimain2003@gmail.com>
2024-07-18 18:53:14 +0530

    chore: Update User model and MainController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+//namespace App\Http\Controllers\Auth;
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\users;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
@@ -54,14 +54,14 @@ public function loginUser(Request $data){
         ]);
     
         try {
-            $newUser = new User();
+            $newUser = new users();
             $newUser->name = $validatedData['name'];
             $newUser->email = $validatedData['email'];
             // Hash the password for security
             $newUser->password = Hash::make($validatedData['password']);
     
             if($newUser->save()){
-                return redirect('/')->with('success', 'Registered successfully!');
+                return redirect('/login')->with('success', 'Registered successfully!');
             } else {
                 // Debug statement to check if save() method returns false
                 dd('Failed to save user');
