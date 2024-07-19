aee9fcba71b6795264ca13c324b68b3ebfb6b723
Maksid23 <makarand.agharkar01@gmail.com>
2024-07-19 10:48:14 +0530

    Added System in main heading

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -275,7 +275,7 @@ nav.close ~ .dashboard .top{
     <!-- Sidebar -->
     <nav id="sidebar">
         <div class="sidebar-header">
-            <h3>Education Management</h3>
+            <h3>Education Management System</h3>
         </div>
 
         <ul class="list-unstyled components">
